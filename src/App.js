0036700541b4b66a3f6bb57fe284d282d8289778
WyldWanderer <living2snowboard@gmail.com>
2021-11-20 20:59:57 -0700

    Adding particle background, more styles to each element and reset button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import Particles from 'react-tsparticles';
 import CategoryButton from './components/CategoryButton';
 import { API_KEY } from './constants'
 
 const App = () => {
   const [categories, addCategory] = useState(["Art & Literature", "Language", "Science & Nature", "General", "Food & Drink", "People & Places", "Geography", "History & Holidays", "Entertainment", "Toys & Games", "Music", "Mathematics", "Religion & Mythology", "Sports & Leisure"])
-  const [question, addQuestion] = useState()
-  const [answer, addAnswer] = useState()
+  const [question, addQuestion] = useState("")
+  const [answer, addAnswer] = useState("")
   const headers = {'X-Api-Key': API_KEY}
 
   const getQuestion = (category) => {
@@ -22,27 +23,127 @@ const App = () => {
         }
       })
       .then((data) => {
-        console.log(data)
         addQuestion(data[0].question)
         addAnswer(data[0].answer)
       })
   }
+
+  const resetQA = () => {
+    addQuestion("")
+    addAnswer("")
+  }
+ 
+  const particlesInit = (main) => {
+    console.log(main);
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   
   
   return (
-    <div>
-      <h1 id="title">Trivia Time!</h1>
-      <h4 id="title">Click on a category below to get a question.</h4>
-      <section id="category-buttons">
-        {categories.map((cat) => {
-          return <CategoryButton category={cat} getQuestion={getQuestion}/>
-        })}
-      </section>
-      <span>
-        <h4 id="question">Question: {question}</h4>
-        <h4 id="answer">Answer: {answer}</h4>
-      </span>
-    </div>
+    <>
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#494949",
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+      <div id="title">
+        <h1>Trivia Time!</h1>
+        <h4>Click on a category below to get a question.</h4>
+      </div>
+      <div>
+        <section id="category-buttons">
+          {categories.map((cat) => {
+            return <CategoryButton category={cat} getQuestion={getQuestion}/>
+          })}
+        </section>
+        <span>
+          <h4 id="question">Question: {question}</h4>
+          <h4 id="answer">Answer: {answer}</h4>
+          <button id="cat-button" onClick={resetQA}>Reset</button>
+        </span>
+      </div>
+    </>
   );
 }
 
